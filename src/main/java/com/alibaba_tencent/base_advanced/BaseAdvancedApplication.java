0cd268f4de9ea59f3a62b9674f8a31e75c76085b
shiwenz <13926108685@163.com>
2020-04-16 08:45:34 +0800

    Java基础与提高

---
+++ src/main/java/com/alibaba_tencent/base_advanced/BaseAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba_tencent.base_advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseAdvancedApplication.class, args);
+    }
+
+}
